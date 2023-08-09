089591e2aea6322f68a22163fc2543029ed07d8f
Almat Sailaukhan <almat.skhan@gmail.com>
2023-08-09 17:51:11 +0600

    unit-tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { FormsModule } from '@angular/forms';
+import { RoutingComponent } from './routing/routing.component'
+import { PostsComponent } from './posts/posts.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ColorDirective } from './directives/color.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    PostsComponent,
+    RoutingComponent,
+    NavbarComponent,
+    ColorDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterTestingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+import {PostsService} from './posts.service';
+
+@Component({
+  template: `Posts component`,
+  selector: 'app-posts'
+})
+export class PostsComponent implements OnInit {
+  posts: any[] = []
+  message!: string
+
+  constructor(private service: PostsService) {
+    
+  }
+
+  ngOnInit(): void {
+    this.service.fetchPromise().then(p => {
+      console.log('fetchPromise CALLED')
+      this.posts = p
+    })
+  }
+
+  add(title: string) {
+    const post = { title }
+    this.service.create(post).subscribe({
+      next: p => {
+        this.posts.push(p)
+      },
+      error: err => this.message = err
+    })
+  }
+
+  delete(id: number) {
+    if (window.confirm('Are you sure?')) {
+      this.service.remove(id).subscribe()
+    }
+  }
+}
